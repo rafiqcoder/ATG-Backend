d359f9808d92ff40be308c3306b2c7e1381de817
rafiqcoder <rafiqcoder@gmail.com>
2022-12-17 18:54:45 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,75 @@
+### ATG Backend Guid
+1. Register
+
+    Online Registration API: https://atg-backend-rafiqcoder.vercel.app/user-registration
+    Local Registration API: http://localhost:5000/register/user-registration
+    Method: Post
+# Data for api call
+    {
+    "firstName" : "Demo",
+    "lastName" : " Name",
+    "email" : "demo@gamil.com",
+    "password" : "demoPassword"
+  }
+
+2. Login
+    Online Registration API: https://atg-backend-rafiqcoder.vercel.app/login
+    Local Registration API: http://localhost:5000/user-login
+
+ # Json Demo Data for Api:
+    {
+        "email" : "demo@gamil.com",
+        "password" : "demoPassword"
+    }
+3. Forget Password
+    Online Registration API: https://atg-backend-rafiqcoder.vercel.app/forgetPassword
+    Local Registration API: http://localhost:5000/forgetPassword
+    Method: Post
+# Json Demo Data for Api:
+    {
+        "email" : "demo@gamil.com"
+    }   
+# Create: Add New Post
+    Online Registration API: https://atg-backend-rafiqcoder.vercel.app/addPost
+    Local Registration API: http://localhost:5000/addPost
+    Method: Post
+    {
+    "title": "Demo is Ready to Test",
+    "description": "This is an example of how to test the application."
+    }
+
+5. Read: Get all post
+
+    Online Registration API: https://atg-backend-rafiqcoder.vercel.app/allPost
+    Local Registration API: http://localhost:5000/allPost
+    Method: Get
+
+6. Delete: Delete post by id
+    Online Registration API: https://atg-backend-rafiqcoder.vercel.app/delete/:id
+    Local Registration API: http://localhost:5000/delete/:id
+    Method: Delete
+
+7. Update: Update post by id
+    Online Registration API: https://atg-backend-rafiqcoder.vercel.app/updatePost/:id
+    Local Registration API: http://localhost:5000/updatePost/:id
+    Method: Patch
+    Need data like this (JSON):
+    
+8. Like: Like post by email
+    Online Registration API: https://atg-backend-rafiqcoder.vercel.app/like/email=demo@gmail.com
+    Local Registration API: http://localhost:5000/like/email=demo@gmail.com
+    Method: Patch
+
+9. Comment:Add new Comment 
+    Online Registration API: https://atg-backend-rafiqcoder.vercel.app/comments
+    Local Registration API: http://localhost:5000/add-comment
+    Method: Post
+# Json Demo Data for Api:
+    {
+    "postId": "",
+    "comments": "text and comments"
+    }
+10. Comment: GET All Comment particular posts by id
+    Online Registration API: https://atg-backend-rafiqcoder.vercel.app/comments/:postId
+    Local Registration API: http://localhost:5000/comments/:postId
+    Method: GET
---
+++ index.js
@@ -0,0 +1,124 @@
+
+const express = require('express');
+const { MongoClient,ServerApiVersion,ObjectId } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1rvc7ql.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri,{ useNewUrlParser: true,useUnifiedTopology: true,serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    const UserList = client.db('ATG_DB').collection('Users');
+    const PostList = client.db('ATG_DB').collection('posts');
+
+    try {
+        app.post('/user-registration',async (req,res) => {
+            const user = req.body;
+            console.log(user);
+            const query = { email: user.email }
+            const existUser = await UserList.find(query).toArray();
+            
+            if (existUser.length === 0) {
+                const result = await UserList.insertOne(user);
+
+                return res.send(result);
+            }
+            res.status(409).send('User already exist');
+        });
+        app.post('/user-login',async (req,res) => {
+            const user = req.body;
+            const query = { email: user.email,password: user.password }
+
+            const userInfo = await UserList.findOne(query);
+
+            if (userInfo) {
+                console.log(userInfo);
+                return res.send({ message: "Logged in Successfully",userInfo });
+            }
+            res.status(401).send('Invalid Credentials');
+        });
+        app.get('/forget-pass',async (req,res) => {
+            const { email } = req.query;
+            const query = { email: email }
+
+            const userInfo = await UserList.findOne(query);
+
+            if (userInfo) {
+                return res.send({ message: 'Password sent to your email' });
+            }
+            res.status(401).send('This email is not registered');
+        });
+        app.post('/add-post',async (req,res) => {
+            const post = req.body;
+            const result = await PostList.insertOne(post);
+            console.log(result);
+            res.send({ message: "posted Successfully",result });
+        });
+        app.get('/get-posts',async (req,res) => {
+            const posts = await PostList.find().toArray();
+            res.send(posts);
+        });
+        app.post('/add-comment',async (req,res) => {
+            const comment = req.body;
+            const query = { _id: ObjectId(comment.postId) }
+            const post = await PostList.findOne(query);
+             if(!post.comments) post.comments = [];
+            const comments = post.comments;
+
+            comments.push(comment);
+            const result = await PostList.updateOne(query,{ $set: { comments: comments } });
+            res.send({ message: "Commented Successfully",totalComments: comments.length });
+        });
+        app.get('/get-comments',async (req,res) => {
+            const { postId } = req.query;
+            const query = { _id: ObjectId(postId) }
+            const post = await PostList.findOne(query);
+            const comments = post.comments;
+            res.send(comments);
+        });
+        
+        app.post('/add-like',async (req,res) => {
+            const like = req.body;
+            const email = like.userEmail;
+            const query = { _id: ObjectId(like.postId) }
+
+            const post = await PostList.findOne(query);
+            if (!post.likes) post.likes = [];
+            if (post.likes.find(like => like.userEmail === email)) {
+                return res.send({
+                    message: "Aleready Liked",
+                    totalLikes: post.likes.length
+                })
+            } 
+            const likes = post.likes;
+            likes.push(like);
+            const result = await PostList.updateOne(query,{ $set: { likes: likes } });
+            res.send({
+                message: "Liked Successfully",
+                totalLikes: post.likes.length
+                ,result
+            });
+
+        });
+
+
+
+
+    } finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
+
+app.listen(port,() => {
+    console.log(`Server is running on port: ${port}`);
+});
